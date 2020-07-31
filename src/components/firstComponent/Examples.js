ad384bfc38bed5b33f0e091b8c91eddb0340e90b
Tureckiy <ev.zhdanov@gmail.com>
2020-07-31 15:52:25 +0300

    tetx

---
+++ src/components/firstComponent/Examples.js
@@ -0,0 +1,14 @@
+import React from "react";
+import sytles from './firstComponent.module.css'   
+import { data } from "./api";
+
+const {} = sytles;
+
+
+export const Examples = ({ }) => {
+
+  data()
+
+
+  return <></>;
+};
---
+++ src/components/firstComponent/api.js
@@ -0,0 +1,41 @@
+import axios from "axios";
+const merch = "2xfWen7fkf1SpT6Ex1r0DM9Xh9h4ruj2";
+const id = 164617;
+const pass = "iRmO4njbxlS0Hn4c6XVb5K1WSbio3B0O";
+const cardnum = 5168755444587899;
+
+const url = "https://api.privatbank.ua/p24api/balance";
+
+const request = `<?xml version="1.0" encoding="UTF-8"?>
+<request version="1.0">
+    <merchant>
+        <id>${id}</id>
+        <signature>${pass}</signature>
+    </merchant>
+    <data>
+        <oper>cmt</oper>
+        <wait>0</wait>
+        <test>0</test>
+        <payment id="">
+        <prop name="cardnum" value=${cardnum} />
+        <prop name="country" value="UA" />
+        </payment>
+    </data>
+</request>
+`;
+ export const data = axios.get(url, (request)).then(console.log)
+
+
+
+
+
+//   .then(function (response) {
+//     console.log(response);
+//   })
+//   .catch(function (error) {
+//     // handle error
+//     console.log(error);
+//   })
+//   .then(function () {
+//     // always executed
+//   });
